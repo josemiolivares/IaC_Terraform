9b0eaa1bb0e9b5a1ac578e267de8e197aa83940a
Josep Olivares <josemiolivares@gmail.com>
2026-05-28 19:32:10 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,8 +31,8 @@ data "aws_availability_zones" "available" {
 
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+#  enable_dns_support   = true
+#  enable_dns_hostnames = true
 
   tags = merge(var.common_tags, {
     Name = "${var.project_name}-vpc"
