2fa5ec1fbe749c405f3ba602af71e6cd9bd70da7
Josep Olivares <josemiolivares@gmail.com>
2026-05-28 22:22:02 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -506,3 +506,56 @@ resource "aws_db_instance" "wordpress_db" {
     Name = "${var.project_name}-db"
   }
 }
+
+
+# SG para la base de datos (RDS)
+resource "aws_security_group" "db_sg" {
+name = "${var.project_name}-db-sg"
+description = "Permitir acceso MySQL desde servidores web"
+vpc_id = aws_vpc.main_vpc.id
+
+ingress {
+description = "MySQL desde Web SG"
+from_port = 3306
+to_port = 3306
+protocol = "tcp"
+security_groups = [aws_security_group.web_sg.id] # solo desde instancias con web_sg
+}
+
+egress {
+from_port = 0
+to_port = 0
+protocol = "-1"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+tags = {
+Name = "${var.project_name}-db-sg"
+}
+}
+
+# SG para EFS (sistema de archivos)
+resource "aws_security_group" "efs_sg" {
+name = "${var.project_name}-efs-sg"
+description = "Permitir acceso NFS (EFS) desde servidores web"
+vpc_id = aws_vpc.main_vpc.id
+ingress {
+description = "NFS desde Web SG"
+from_port = 2049
+to_port = 2049
+protocol = "tcp"
+security_groups = [aws_security_group.web_sg.id]
+}
+
+egress {
+from_port = 0
+to_port = 0
+protocol = "-1"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+
+tags = {
+Name = "${var.project_name}-efs-sg"
+}
+}
