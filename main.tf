1069aa41507dd797d6ed9e9231969f680085f709
Josep Olivares <josemiolivares@gmail.com>
2026-05-28 19:57:49 +0200

    Change AMI ID and adjust instance type configuration
    Updated AMI ID and modified instance type assignment.

--- main.tf
+++ main.tf
@@ -263,10 +263,10 @@ resource "aws_key_pair" "main" {
 
 resource "aws_instance" "web" {
   count = 2
-  ami           = "ami-0c101f26f147fa7fd"
-  instance_type = "t3.micro"
+  ami           = "ami-00e801948462f718a"
+ # instance_type = "t3.micro"
  # ami                    = data.aws_ami.amazon_linux.id
- # instance_type          = var.instance_type
+  instance_type          = var.instance_type
   key_name               = aws_key_pair.main.key_name
   subnet_id              = aws_subnet.private[count.index].id
   vpc_security_group_ids = [aws_security_group.ec2.id]
